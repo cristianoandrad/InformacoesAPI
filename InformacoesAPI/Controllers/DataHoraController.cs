e9a55c880366a16d839bd164f2361253105e7fb8
cristianoandrad <cristianocontabil@gmail.com>
2021-07-20 12:44:29 -0300

    primeiro commit

---
+++ InformacoesAPI/Controllers/DataHoraController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace InformacoesAPI.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataHoraController : ControllerBase
+    {
+        public string data = DateTime.Now.ToString("yyyy-MM-dd");
+        public string hora = DateTime.Now.ToString("t");
+        public List<string> info = new List<string>();
+        
+                   
+
+        /// <summary>
+        /// Informa data e hora.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+
+        public ActionResult Get()
+        {
+            
+            info.Add(data);
+            info.Add(hora);
+            return BadRequest(info);
+        }
+
+       
+    }
+}
---
+++ InformacoesAPI/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InformacoesAPI.Controllers
+{
+    /// <summary>
+    /// Informe usuario e senha.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private string usuario = "admin";
+        private string senha = "admin";
+        private string info;
+
+       
+        /// <summary>
+        /// Informe usuario e senha.
+        /// </summary>
+        /// <param name="usuario"></param>
+        /// <param name="senha"></param>
+        /// <returns></returns>
+        [HttpPost]
+
+        public ActionResult Post(string usuario, string senha)
+        {
+            info = (usuario == "admin" && senha == "admin" ? "Seja bem vindo!": "Usuario e senha inválidos.");
+            return Ok(info);
+        }
+
+    }
+}
